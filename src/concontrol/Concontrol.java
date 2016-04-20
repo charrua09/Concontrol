6693ac9f4ba5c1c7bd1715eaaa5a42b3abffd392
charrua09 <charrua09@users.noreply.github.com>
2016-04-20 17:55:15 -0500

    Update Concontrol.java
    aumentó el saludo

--- src/concontrol/Concontrol.java
+++ src/concontrol/Concontrol.java
@@ -27,7 +27,7 @@ public class Concontrol {
         System.out.println("\nFact (3)= " + fact(3) + "\n");
         System.out.println("\nLa suma de 4+5 es: " + suma(4,5));
         
-        System.out.println("FIN");
+        System.out.println("FIN de esta tarde gris");
     }
     
     
