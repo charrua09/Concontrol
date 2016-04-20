5414cd0b7d39b3bd8f9d69d72f337b464c2af70a
charrua09 <charrua09@users.noreply.github.com>
2016-04-20 17:12:54 -0500

    Added files via upload

---
+++ Concontrol.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package concontrol;
+
+/**
+ *
+ * @author sguardatb
+ */
+public class Concontrol {
+
+    public static int fact(int n){
+        int f = 1;
+        for (int i= 2; i < n; i++)
+            f= f*i;
+        return f;
+    }
+    
+    public static int suma(int n1, int n2){
+        return n1+n2;
+    }
+    public static void main(String[] args) {
+        System.out.println("Hola");
+        System.out.println("\nFact (3)= " + fact(3) + "\n");
+        System.out.println("\nLa suma de 4+5 es: " + suma(4,5));
+    }
+    
+}
